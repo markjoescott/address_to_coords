4bb5cd0c1bdb818397141591961f6a8a6efecb4f
markjoescott <m-scott@kellogg.northwestern.edu>
2014-05-03 10:11:35 -0500

    Cont. working on API Project 1
    Completed everything except the last challenge - getting localhost3000
    to show coords

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,8 +3,10 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
+    # @address = "the corner of Foster and Sheridan"
+    @address = params[:address]
     @url_safe_address = URI.encode(@address)
+
 
     # Your code goes here.
     url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}+&sensor=false"
