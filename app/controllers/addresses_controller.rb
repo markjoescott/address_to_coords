ab7d68d81efd29f31de10f851670cb3f715004bc
markjoescott <m-scott@kellogg.northwestern.edu>
2014-05-05 13:02:52 -0500

    Finished address to coords
    added if/then logic to successfully complete API Project 1

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,14 +5,22 @@ class AddressesController < ApplicationController
   def fetch_coordinates
     # @address = "the corner of Foster and Sheridan"
     @address = params[:address]
-    @url_safe_address = URI.encode(@address)
-
+    if @address == nil
+      @url_safe_address = URI.encode("the corner of Foster and Sheridan")
+    else
+      @url_safe_address = URI.encode(@address)
+    end
 
     # Your code goes here.
     url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}+&sensor=false"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
-    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
-    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    if parsed_data["results"][0] != nil
+        @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+        @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    else
+        @latitude = 0.0
+        @longitude = 0.0
+    end
   end
 end
